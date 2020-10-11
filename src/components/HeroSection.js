971cbbe7350482a7b592dc2a4872cfab6d371813
Alice Hata <alicehata01@hotmail.com>
2020-10-11 19:20:12 -0300

    React Website
    Create page with briancodex tutorial.
    Used react-icons with font awesome icons

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../App.css';
+import './HeroSection.css'
+import { FaPlayCircle } from 'react-icons/fa'
+import { Button }  from './Button'
+
+export default function HeroSection() {
+    return (
+        <div className='hero-container'>
+            {/* <video src='/videos/video-2.mp4' autoPlay loop muted/> */}
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className='hero-btns'>
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                    GET STARTED
+                </Button> 
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                    WATCH TRAILER <FaPlayCircle className='fa-play-circle'/>
+                </Button> 
+            </div>
+        </div>
+    )
+}

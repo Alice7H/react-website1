d42d05cd32b80095079087794b0dc03270f69dce
Alice Hata <alicehata9@gmail.com>
2022-06-14 17:27:16 -0300

    Updates

---
+++ precache-manifest.69d0d2f74ee4b857a1b5422096761644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba75868ca23f294d87f9d8893749341",
+    "url": "/react-website1/index.html"
+  },
+  {
+    "revision": "08fb509c5085f0f62cb3",
+    "url": "/react-website1/static/css/main.7d2f30b5.chunk.css"
+  },
+  {
+    "revision": "85e5d5b353601279c210",
+    "url": "/react-website1/static/js/2.c6a22571.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website1/static/js/2.c6a22571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08fb509c5085f0f62cb3",
+    "url": "/react-website1/static/js/main.ebeb8c9c.chunk.js"
+  },
+  {
+    "revision": "8b56ae4dd303e83c3313",
+    "url": "/react-website1/static/js/runtime-main.b5e43d76.js"
+  }
+]);
